e52dbcd844826adcda1bb76df037f95e0f7ce017
Blai Samitier <blai.samitier@gmail.com>
2015-12-07 13:59:53 +0100

    first routes & controllers

--- routes.js
+++ routes.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var ctrl = require('./controllers');
 
-/* GET home page. */
+
+/* GET API info. */
 router.get('/', function(req, res, next) {
-  res.end('This is the API');
+    res.end('This is the web API for Smarter Tourism');
 });
 
+
+/* Acivities */
+router.get('/activities', ctrl.activities.getAll);
+router.post('/activities', ctrl.activities.create);
+
+
+/* Not found, for every other route */
+router.all('*', function(req, res) {res.send({ error: '404-Not found' });});
+
+
 module.exports = router;
+
