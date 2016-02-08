5cc992444536fa0365fbf121041cfcf18c8136bc
Blai Samitier <blai@samitier.me>
2016-02-08 11:41:49 +0100

    more paypal config

--- models/Order.js
+++ models/Order.js
@@ -6,6 +6,7 @@ var OrderSchema = new mongoose.Schema({
     seller: {type:mongoose.Schema.Types.ObjectId, ref: 'User' , required:true},
     title: {type: String, required:true},
     product: {
+        id: {type:mongoose.Schema.Types.ObjectId},
         title: {type: String, required:true},
         variation: {type: String, required:true},
         extra: {type: String},
